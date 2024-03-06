7526d4999d4b8a7eb361a1a384a98b204517391f
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-06 13:09:39 -0600

    added quote icons

--- app/components/blocks/Timeline/TimelineItem.jsx
+++ app/components/blocks/Timeline/TimelineItem.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import quoteIcon from "/public/icons/quote-icon.png";
+
 export default function TimelineItem({
   id,
   year,
@@ -14,15 +17,19 @@ export default function TimelineItem({
         <div className="w-4 h-4 rounded-full bg-black dark:bg-white"></div>
       </div>
 
-      <div className=" text-balance col-start-3">
-        <h3>{title}</h3>
-        <p>{description}</p>
+      <div className="text-balance col-start-3 flex flex-col gap-8">
+        <div>
+          <h3 className="mb-4">{title}</h3>
+          <p>{description}</p>
+        </div>
 
-        <div className="flex flex-col bg-gray-200 relative">
+        <div className="flex flex-col bg-gray-200 relative p-6 rounded-lg">
           <span>{quote}</span>
           <span className="text-blue-base">{quoteAuthor}</span>
 
-          <div></div>
+          <div className="absolute -top-6 -right-4 dark:invert">
+            <Image src={quoteIcon} alt="" width={60} height={60} />
+          </div>
         </div>
       </div>
     </div>
