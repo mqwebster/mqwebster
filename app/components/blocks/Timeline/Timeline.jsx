dd2ff01b7af5d8a3307c3817d8adc85689ba4520
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-06 13:09:52 -0600

    updated timeline layout

--- app/components/blocks/Timeline/Timeline.jsx
+++ app/components/blocks/Timeline/Timeline.jsx
@@ -3,14 +3,16 @@ import { timelineData } from "./timelineData.js";
 
 export default function Timeline() {
   return (
-    <div>
-      {/* <div className="bg-gray-200 w-1 h-full absolute top-0">
-        <div className="bg-blue-300 w-1 h-1/2 fixed bottom-1/2"></div>
-      </div> */}
+    <div className="relative flex flex-col items-center">
+      <div className="bg-gray-200 w-1 h-full absolute top-0 -z-10">
+        <div className="bg-blue-base w-1 h-1/2 fixed top-0"></div>
+      </div>
 
-      {timelineData.map((item) => {
-        return <TimelineItem key={item.id} {...item} />;
-      })}
+      <div className="flex flex-col gap-16 z-0">
+        {timelineData.map((item) => {
+          return <TimelineItem key={item.id} {...item} />;
+        })}
+      </div>
     </div>
   );
 }
