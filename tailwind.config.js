665f476654c516ae02952af1ab47ad1401e99af5
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-06 11:37:50 -0600

    added custom blue and yellow colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue: {
+          base: "#228EE8",
+          dark: "#004488",
+        },
+        yellow: {
+          base: "#EBDF2B",
+          dark: "#AAA800",
+        },
+      },
     },
   },
   plugins: [],
