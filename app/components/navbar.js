c73bc21728c072417f50d394907c250cedf8105c
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-06 16:28:33 -0600

    added max height to navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
     { link: "#contact", text: "Contact" },
   ];
   return (
-    <nav id="navbar" className="px-16 py-4 bg-blue-base">
+    <nav id="navbar" className="max-h-[112px] px-16 py-4 bg-blue-base">
       <div className="mx-auto flex flex-row justify-between items-center max-w-screen-xl">
         <Image
           src="/MarquesWebster.png"
