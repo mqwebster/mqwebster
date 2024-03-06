28a72124d83faf531f2430cae2d3a2fe26a17bba
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-06 15:54:14 -0600

    added initial Home section

---
+++ app/components/sections/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <section id="#home" className="w-full max-w-screen-xl z-10">
+      <div className="min-h-[92vh] flex flex-col justify-center items-center">
+        <span className="">{`Hey, I'm...`}</span>
+        <div className="">
+          <ul className="">
+            <li>Marques Q. Webster</li>
+          </ul>
+        </div>
+        <h1>{`I'm a Frontend Software Developer!`}</h1>
+      </div>
+    </section>
+  );
+}
