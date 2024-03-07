2c0ce77f663f85bda599c63e64558bb9063db369
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-07 15:18:47 -0600

    updated Home section content

--- app/components/sections/Home.tsx
+++ app/components/sections/Home.tsx
@@ -30,13 +30,12 @@ export function Home() {
 export const Header = () => {
   return (
     <div className="max-w-screen-xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
+      <span>{`Hello, I'm...`}</span>
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        Marques Webster
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+        And I love to build cool shit.
       </p>
     </div>
   );
