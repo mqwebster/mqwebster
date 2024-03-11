5c7ff65767b69d888eef881147eae43a49ef701c
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-10 20:12:50 -0500

    increased font size on mobile

--- app/components/sections/Home.tsx
+++ app/components/sections/Home.tsx
@@ -31,10 +31,8 @@ export const Header = () => {
   return (
     <div className="max-w-screen-xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <span>{`Hello, I'm...`}</span>
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Marques Webster
-      </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+      <h1 className="text-7xl font-bold dark:text-white">Marques Webster</h1>
+      <p className="max-w-2xl text-xl mt-8 dark:text-neutral-200">
         And I love to build cool shit.
       </p>
     </div>
