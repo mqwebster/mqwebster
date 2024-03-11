311acbbb9412d6456fb2c3e21bfa89371e69ff87
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-10 23:02:27 -0500

    scroll positioning for timeline year and center dot

--- app/components/blocks/Timeline/TimelineItem.tsx
+++ app/components/blocks/Timeline/TimelineItem.tsx
@@ -17,11 +17,13 @@ export default function TimelineItem({
   quoteAuthor: string;
 }) {
   return (
-    <div className="grid gap-4 lg:gap-16 grid-cols-[16%,max-content,1fr] lg:grid-cols-[1fr,max-content,1fr] mb-16">
-      <div className="md:ml-auto">{year}</div>
+    <div className="grid gap-4 lg:gap-16 grid-cols-[16%,max-content,1fr] lg:grid-cols-[1fr,max-content,1fr] mb-16 relative">
+      <div className="md:ml-auto h-full">
+        <span className="sticky top-1/2">{year}</span>
+      </div>
 
       <div className="flex justify-center">
-        <div className="w-4 h-4 rounded-full bg-black dark:bg-white"></div>
+        <div className="w-4 h-4 rounded-full bg-black dark:bg-white sticky top-1/2"></div>
       </div>
 
       <div className="text-balance col-start-3 flex flex-col gap-8">
