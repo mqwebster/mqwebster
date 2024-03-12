3e81da46d2192aa54ce1e1d6f1413a88447046aa
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-12 17:49:47 -0500

    updated card height and image sizing

--- app/components/blocks/Portfolio/PortfolioItem.tsx
+++ app/components/blocks/Portfolio/PortfolioItem.tsx
@@ -19,19 +19,21 @@ export default function PortfolioItem({
   return (
     <div
       key={`ProjectItem-${id}`}
-      className="w-full h-[26rem] font-body type-preset-base"
+      className="w-full h-[32rem] font-body type-preset-base"
     >
-      <PinContainer title={title} href={link}>
-        <div className="flex flex-col p-4 tracking-tight text-slate-100/50">
-          <h4 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100 dark:text-slate-900 font-body type-preset-lg">
-            {title}
-          </h4>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-300 dark:text-slate-500 font-body type-preset-base">
-              {description}
-            </span>
+      <PinContainer title={title} href={link} className="h-full">
+        <div className="flex flex-col justify-between p-4 tracking-tight text-slate-100/50 h-full">
+          <div>
+            <h4 className="font-bold font-body type-preset-base text-slate-100 dark:text-slate-900 mb-2">
+              {title}
+            </h4>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-300 dark:text-slate-500 font-body type-preset-base">
+                {description}
+              </span>
+            </div>
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg overflow-clip aspect-video">
+          <div className="flex rounded-lg mt-4 bg overflow-clip aspect-video">
             <Image src={image} alt={`${title} screenshot`} />
           </div>
         </div>
