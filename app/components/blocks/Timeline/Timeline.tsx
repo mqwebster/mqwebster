2d24a0cb7c68ee81e0730f51e1eda1a6b5c2a549
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-13 14:04:06 -0500

    updated timeline item element positioning

--- app/components/blocks/Timeline/Timeline.tsx
+++ app/components/blocks/Timeline/Timeline.tsx
@@ -11,7 +11,7 @@ export default function Timeline() {
       {/* +.5rem = TimelineItem center column width/Dot */}
       {/* -.125rem = half of Timeline inner progress bar. Subtracted to move bar in line with middle of Grid center */}
       <div className="tl-progress-bar bg-gray-200 w-1 h-full absolute top-0 left-[calc(16%+1.375rem)] lg:left-[calc(16%+4.375rem)] -z-10">
-        <div className="bg-blue-base w-1 h-[51%] fixed top-0 rounded-b-lg"></div>
+        <div className="bg-blue-base w-1 h-[54%] md:h-[55%] fixed top-0 rounded-b-lg"></div>
       </div>
 
       <div className="flex flex-col gap-16 z-0">
--- app/components/blocks/Timeline/TimelineItem.tsx
+++ app/components/blocks/Timeline/TimelineItem.tsx
@@ -20,12 +20,12 @@ export default function TimelineItem({
     <div
       className={`grid gap-4 lg:gap-16 grid-cols-[16%,max-content,1fr] mb-16 relative font-body type-preset-base`}
     >
-      <div className="md:ml-auto h-full">
-        <span className="sticky top-1/2">{year}</span>
+      <div className="h-full">
+        <span className="sticky top-1/2 font-bold type-preset-2">{year}</span>
       </div>
 
-      <div className="flex justify-center">
-        <div className="w-4 h-4 rounded-full bg-black dark:bg-white sticky top-1/2"></div>
+      <div className="flex justify-center my-4 md:my-8">
+        <div className="w-4 h-4 rounded-full bg-black dark:bg-white sticky top-[52%] md:top-[54%]"></div>
       </div>
 
       <div className="text-balance col-start-3 flex flex-col gap-8">
