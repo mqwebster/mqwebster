7db06bce4327497714616f676f1e9bdf775e59e7
HI IQ Designs <hiiqdesigns@gmail.com>
2024-04-16 23:21:19 -0500

    made inner and outer Timeline progress bar indicators fully rounded

--- app/components/blocks/Timeline/Timeline.tsx
+++ app/components/blocks/Timeline/Timeline.tsx
@@ -10,8 +10,8 @@ export default function Timeline() {
       {/* +1rem/4rem = Grid column gap */}
       {/* +.5rem = TimelineItem center column width/Dot */}
       {/* -.125rem = half of Timeline inner progress bar. Subtracted to move bar in line with middle of Grid center */}
-      <div className="tl-progress-bar bg-gray-200 w-1 h-full absolute top-0 left-[calc(16%+1.375rem)] lg:left-[calc(16%+4.375rem)] -z-10">
-        <div className="bg-blue-base w-1 h-[54%] md:h-[55%] fixed top-0 rounded-b-lg"></div>
+      <div className="tl-progress-bar bg-gray-200 w-1 h-full absolute top-0 left-[calc(16%+1.375rem)] lg:left-[calc(16%+4.375rem)] -z-10 rounded-lg">
+        <div className="bg-blue-base w-1 h-[54%] md:h-[55%] fixed top-0 rounded-lg"></div>
       </div>
 
       <div className="flex flex-col gap-16 z-0">
