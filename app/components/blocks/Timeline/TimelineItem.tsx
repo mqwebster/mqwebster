6a108e04a53669634091703828cd5f7c51baecfa
HI IQ Designs <hiiqdesigns@gmail.com>
2024-04-18 23:30:38 -0500

    updated Timeline layout

--- app/components/blocks/Timeline/TimelineItem.tsx
+++ app/components/blocks/Timeline/TimelineItem.tsx
@@ -18,18 +18,25 @@ export default function TimelineItem({
 }) {
   return (
     <div
-      className={`grid gap-4 lg:gap-16 grid-cols-[16%,max-content,1fr] mb-16 relative font-body type-preset-base`}
+      className={`grid grid-cols-[1rem, 1fr] md:grid-cols-3 gap-4 md:gap-16 mb-16 relative font-body type-preset-base`}
     >
-      <div className="h-full">
-        <span className="sticky top-1/2 font-bold type-preset-2">{year}</span>
+      {/* <div className="h-full">
+      </div> */}
+
+      <div className="w-full col-start-1 col-span-1 flex relative">
+        <div className="w-full h-max flex justify-between items-center sticky top-1/2">
+          <span className="font-bold type-preset-2 hidden md:block">
+            {year}
+          </span>
+          <div className="w-4 h-4 rounded-full bg-black dark:bg-white mt-4 md:mt-0"></div>
+        </div>
       </div>
 
-      <div className="flex justify-center my-4 md:my-8">
-        <div className="w-4 h-4 rounded-full bg-black dark:bg-white sticky top-[52%] md:top-[54%]"></div>
-      </div>
-
-      <div className="text-balance col-start-3 flex flex-col gap-8">
+      <div className="text-balance col-start-2 col-span-2 flex flex-col gap-8 md:mt-2">
         <div>
+          <span className="font-bold type-preset-2 block md:hidden mb-4">
+            {year}
+          </span>
           <h3 className="font-title type-preset-3 mb-5">{title}</h3>
           <p>{description}</p>
         </div>
