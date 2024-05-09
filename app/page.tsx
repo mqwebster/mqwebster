3b1c125d35261864fb556e76890889765b9f367e
Marques Webster <marques.q.webster@gmail.com>
2024-05-08 20:04:44 -0500

    no change

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Contact } from "./components/sections/Contact";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       <Home />
 
       <About />
