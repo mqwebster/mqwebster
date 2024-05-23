de2b1e1252ebea6a1807546d61091dec8cd40dab
Marques Webster <marques.q.webster@gmail.com>
2024-05-22 23:01:38 -0500

    setup portfolio to have dropdown functionality and moved the dropdown to it's own reusable component

---
+++ app/components/blocks/Portfolio/PortfolioDropdown.tsx
@@ -0,0 +1,51 @@
+import { portfolioData } from "@/app/data/portfolioData";
+import PortfolioItem from "./PortfolioItem";
+
+export const PortfolioDropdown = ({
+  category,
+  body = null,
+  isOpen,
+}: {
+  category: string;
+  body?: string;
+  isOpen: boolean;
+}) => {
+  const categoryWords = category.split(" ");
+  for (let i = 0; i < categoryWords.length; i++) {
+    categoryWords[i] =
+      categoryWords[i][0].toUpperCase() + categoryWords[i].substring(1);
+  }
+  const capitalCategory = categoryWords.join(" ");
+
+  return (
+    <details className="group" open={isOpen}>
+      <summary className="flex justify-between items-center pr-4 list-none border-b-2 border-blue-200 cursor-pointer">
+        <h3 className="font-title type-preset-3">{capitalCategory}</h3>
+
+        <ArrowIcon />
+      </summary>
+
+      {body && <p className="mt-5">{body}</p>}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-8 my-16">
+        {portfolioData[category].map((item, index) => {
+          return (
+            <div key={`PortfolioItem-${index}_${item.title}`}>
+              <PortfolioItem {...item} />
+            </div>
+          );
+        })}
+      </div>
+    </details>
+  );
+};
+
+const ArrowIcon = () => {
+  return (
+    <div className="flex flex-col justify-start items-center w-3 h-3 relative">
+      <div className="w-full h-[2px] bg-black dark:bg-blue-200 absolute -rotate-45 origin-top-right rounded-lg ml-[0.1875rem] group-open:ml-0 group-open:-translate-x-[0.375rem] group-open:translate-y-[0.375rem] transition-all duration-500 delay-0"></div>
+
+      <div className="w-full h-[2px] bg-black dark:bg-blue-200 absolute rotate-45 origin-top-left rounded-lg mr-[0.1875rem] group-open:mr-0 group-open:translate-x-[0.375rem] group-open:translate-y-[0.375rem] transition-all duration-500 delay-100"></div>
+    </div>
+  );
+};
--- app/sections/Portfolio.tsx
+++ app/sections/Portfolio.tsx
@@ -1,5 +1,4 @@
-import { portfolioData } from "../data/portfolioData";
-import PortfolioItem from "../components/blocks/Portfolio/PortfolioItem";
+import { PortfolioDropdown } from "../components/blocks/Portfolio/PortfolioDropdown";
 
 export function Portfolio() {
   return (
@@ -11,52 +10,15 @@ export function Portfolio() {
         <h2 className="font-title type-preset-2 mb-10">The What...</h2>
 
         <div className="flex flex-col gap-10">
-          <div>
-            <h3 className="font-title type-preset-3">Sites</h3>
+          <PortfolioDropdown category="sites" isOpen={true} />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-8 my-16">
-              {portfolioData["sites"].map((item, index) => {
-                return (
-                  <div key={`SiteItem-${index}_${item.title}`}>
-                    <PortfolioItem key={`SiteItem-${item.title}`} {...item} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <PortfolioDropdown
+            category="design systems"
+            body="Artifacts from recent projects I’ve worked on. Take a look at what I’ve put together."
+            isOpen={false}
+          />
 
-          <div>
-            <h3 className="font-title type-preset-3 mb-5">Design Systems</h3>
-
-            <p>
-              Artifacts from recent projects I’ve worked on. Take a look at what
-              I’ve put together.
-            </p>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-8 my-16">
-              {portfolioData["design systems"].map((item, index) => {
-                return (
-                  <div key={`SiteItem-${index}_${item.title}`}>
-                    <PortfolioItem key={`SiteItem-${item.title}`} {...item} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          <div>
-            <h3 className="font-title type-preset-3 mb-5">Projects</h3>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-8 my-16">
-              {portfolioData["projects"].map((item, index) => {
-                return (
-                  <div key={`SiteItem-${index}_${item.title}`}>
-                    <PortfolioItem key={`SiteItem-${item.title}`} {...item} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <PortfolioDropdown category="projects" isOpen={false} />
         </div>
       </div>
     </section>
