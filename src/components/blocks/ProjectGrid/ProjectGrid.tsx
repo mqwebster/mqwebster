f4a94a10291afed1a527b055fb5bd05344c756c4
Marques Webster <marques.q.webster@gmail.com>
2024-11-21 20:34:09 -0600

    added support for bento grid on Project Grid component

--- src/components/blocks/ProjectGrid/ProjectGrid.tsx
+++ src/components/blocks/ProjectGrid/ProjectGrid.tsx
@@ -1,5 +1,6 @@
 import { ProjectGridInterface } from "@/types/blocks/ProjectGridInterface";
 import ProjectCard from "../../atoms/ProjectCard";
+import { BentoGrid, BentoGridItem } from "../../ui/bento-grid";
 
 function ProjectGrid({
   id,
@@ -8,16 +9,35 @@ function ProjectGrid({
 }: ProjectGridInterface) {
   return (
     <section className="w-full md:max-w-screen-xl z-0 px-8 py-8">
-      <div className="w-full grid grid-cols-2 gap-8">
-        {/* @ts-ignore */}
-        {projectListCollection?.items.map((project) => {
-          return (
-            <div key={project.title}>
+      {type === "Bento" ? (
+        <div>
+          <BentoGrid className="mx-auto">
+            {/* @ts-ignore */}
+            {projectListCollection?.items.map((item, i) => {
+              return (
+                <BentoGridItem
+                  key={item.sys.id}
+                  id={item.sys.id}
+                  title={item.title}
+                  description={item.description}
+                  className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+                />
+              );
+            })}
+          </BentoGrid>
+        </div>
+      ) : (
+        <div className="w-full grid grid-cols-2 gap-8">
+          {/* @ts-ignore */}
+          {projectListCollection?.items.map((project) => {
+            return (
+              <div key={project.title}>
                 <ProjectCard id={project.sys.id} gridType={type} {...project} />
-            </div>
-          );
-        })}
-      </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 }
