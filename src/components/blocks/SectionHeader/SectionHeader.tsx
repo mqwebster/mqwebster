9d3c88566551b91dcd4f8e395e618fee521de437
Marques Webster <marques.q.webster@gmail.com>
2024-11-21 20:36:35 -0600

    updated section header layout

--- src/components/blocks/SectionHeader/SectionHeader.tsx
+++ src/components/blocks/SectionHeader/SectionHeader.tsx
@@ -10,13 +10,11 @@ function SectionHeader({
   color,
 }: SectionHeaderInterface) {
   return (
-    <section id="" className="w-full md:max-w-screen-xl z-0 px-8 py-8">
-      <div className="flex flex-col pb-4">
-        <div className="w-full dark:bg-black">
-          <h2 className="font-title type-preset-2 mb-10">{title}</h2>
-          <div className="max-w-4xl">
-            <RichBodyContent body={body} />
-          </div>
+    <section id="" className="w-full md:max-w-screen-xl z-0 px-8 pb-8 pt-16">
+      <div className="flex flex-col">
+        <h2 className="font-title type-preset-2">{title}</h2>
+        <div className="max-w-4xl">
+          {body && <RichBodyContent body={body} />}
         </div>
       </div>
     </section>
