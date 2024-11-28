d15baa417f02dc5144554644b09b4dbc8ac2149f
Marques Webster <marques.q.webster@gmail.com>
2024-11-27 23:52:11 -0600

    updated navbar design and layout

--- src/components/blocks/navbar.tsx
+++ src/components/blocks/navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
   const bgColor = useTransform(
     scrollYProgress,
     [0, 0.18, 0.2],
-    ["#228ee800", "#228ee800", "#228ee8ff"]
+    ["#228ee800", "#228ee800", "#228ee8c4"]
   );
   const fgColor = useTransform(
     scrollYProgress,
@@ -40,77 +40,77 @@ export default function Navbar() {
   );
 
   return (
-    <motion.nav
+    <nav
       id="navbar"
-      style={{ backgroundColor: bgColor }}
-      className={`w-full py-4 sticky top-0 z-50 ${menu && "!bg-blue-base"}`}
+      className={`w-full py-4 sticky top-0 z-50 ${
+        menu
+          ? ""
+          : "bg-gradient-to-b from-[#f0f0f0] dark:from-black to-transparent"
+      }`}
     >
-      <div className="flex justify-between items-center md:max-w-screen-xl px-8 mx-auto">
+      <motion.div
+        style={{ backgroundColor: menu ? "transparent" : bgColor }}
+        className={`nav-container relative z-10 flex justify-between items-center md:max-w-screen-xl px-8 py-2 mx-1 lg:mx-auto rounded-lg ${
+          menu ? "backdrop-blur-none" : "backdrop-blur-md"
+        }`}
+      >
         <Link href={`/`}>
           <Image
             src="/images/MarquesWebster.png"
             alt="Marques Webster"
-            width={60}
-            height={60}
+            width={48}
+            height={48}
           />
         </Link>
-
-        <div>
-          <div
-            className="flex items-center h-[48px] cursor-pointer"
-            onClick={toggle}
-          >
-            <div className="relative flex flex-col justify-center items-end gap-2 w-[48px]">
-              <motion.div
-                style={{ backgroundColor: fgColor }}
-                className={`w-2/3 h-1 dark:!bg-white rounded-lg transition-transform duration-500 ${
-                  menu &&
-                  "absolute -rotate-[405deg] origin-center top-0 !bg-white"
-                }`}
-              ></motion.div>
-              <motion.div
-                style={{ backgroundColor: fgColor }}
-                className={`w-full h-1 dark:!bg-white rounded-lg transition-transform duration-500 ${
-                  menu && " scale-0 bg-transparent !bg-white"
-                }`}
-              ></motion.div>
-              <motion.div
-                style={{ backgroundColor: fgColor }}
-                className={`w-2/3 h-1 dark:!bg-white rounded-lg transition-transform duration-500 ${
-                  menu &&
-                  "absolute rotate-[405deg] origin-center bottom-0 !bg-white"
-                }`}
-              ></motion.div>
-            </div>
+        <div
+          className="flex items-center h-[48px] cursor-pointer"
+          onClick={toggle}
+        >
+          <div className="relative flex flex-col justify-center items-end gap-[0.125rem] w-[40px]">
+            <motion.div
+              style={{ backgroundColor: fgColor }}
+              className={`w-full h-[0.125rem] dark:!bg-white rounded-lg transition-all duration-300 ${
+                menu &&
+                "absolute -rotate-[225deg] origin-center top-0 !bg-white"
+              }`}
+            ></motion.div>
+            <div className={`w-full h-[0.125rem] bg-transparent`}></div>
+            <motion.div
+              style={{ backgroundColor: fgColor }}
+              className={`w-full h-[0.125rem] dark:!bg-white rounded-lg transition-all duration-300 ${
+                menu &&
+                "absolute rotate-[225deg] origin-center bottom-0 !bg-white"
+              }`}
+            ></motion.div>
           </div>
-
-          {menu && (
-            <div
-              className={`absolute top-full left-0 w-full h-screen pb-16 overflow-scroll bg-blue-base blueScrollbar`}
-            >
-              <div className="flex justify-between items-center md:max-w-screen-xl px-8 mx-auto">
-                <ul className="flex flex-col gap-2 type-preset-2 font-title text-white py-8">
-                  {navItems.map((item) => {
-                    return (
-                      <li
-                        key={item.text}
-                        className="group/navItem flex flex-col w-fit"
-                      >
-                        <Link href={`${item.link}`} onClick={toggle}>
-                          {item.text}
-                        </Link>
-                        <div
-                          className={`h-3 w-0 rounded-lg group-hover/navItem:w-full bg-white transition-[width] duration-500`}
-                        ></div>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            </div>
-          )}
         </div>
-      </div>
-    </motion.nav>
+      </motion.div>
+
+      {menu && (
+        <div
+          className={`absolute top-0 left-0 z-0 w-full h-screen pt-[96px] pb-16 overflow-scroll bg-blue-base bg-opacity-[0.77] backdrop-blur-lg blueScrollbar`}
+        >
+          <div className="flex justify-between items-center md:max-w-screen-xl px-8 mx-auto">
+            <ul className="flex flex-col gap-2 type-preset-2 font-title text-white py-8">
+              {navItems.map((item) => {
+                return (
+                  <li
+                    key={item.text}
+                    className="group/navItem flex flex-col w-fit"
+                  >
+                    <Link href={`${item.link}`} onClick={toggle}>
+                      {item.text}
+                    </Link>
+                    <div
+                      className={`h-1 w-0 rounded-lg group-hover/navItem:w-full bg-white transition-[width] duration-500`}
+                    ></div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
