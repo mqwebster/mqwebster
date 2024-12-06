20318301a291436c6ccef23229327ae12dba4c72
Marques Webster <marques.q.webster@gmail.com>
2024-12-06 10:34:17 -0600

    added support for slugs on page gql client queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { draftMode } from "next/headers";
 import { client, previewClient } from "@/src/lib/client";
 import { PageInterface } from "@/types/PageInterface";
-import BaseTemplate from "../templates/BaseTemplate";
+import BaseTemplate from "@/src/templates/BaseTemplate";
 
 export default async function Page() {
   const { isEnabled: preview } = draftMode();
   const gqlClient = preview ? previewClient : client;
 
-  const landingPageData = await gqlClient.Page({ preview, locale: "en-US" });
+  const landingPageData = await gqlClient.Page({
+    slug: "/",
+    preview,
+    locale: "en-US",
+  });
   // @ts-ignore
   const page: PageInterface = landingPageData.pageCollection?.items[0];
 
