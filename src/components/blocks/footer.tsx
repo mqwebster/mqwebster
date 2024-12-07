b532c76d2a606ad73c4bc556bdc93f625096991e
Marques Webster <marques.q.webster@gmail.com>
2024-12-06 20:27:09 -0600

    turned visitor count back on in footer

--- src/components/blocks/footer.tsx
+++ src/components/blocks/footer.tsx
@@ -1,26 +1,20 @@
 "use client";
 import { useEffect, useState } from "react";
-
-import download from "@/public/icons/download-icon.png";
-import email from "@/public/icons/email-icon.png";
-import github from "@/public/icons/github-icon.png";
-import linkedin from "@/public/icons/linkedin-icon.png";
-
 import Button from "@/src/components/atoms/Button";
 
 export default function Footer() {
   const [visitors, setVisitors] = useState("");
 
-  // useEffect(() => {
-  //   const apiURL =
-  //     "https://rty7kmnenc.execute-api.us-east-2.amazonaws.com/api/siteViewCount";
+  useEffect(() => {
+    const apiURL =
+      "https://rty7kmnenc.execute-api.us-east-2.amazonaws.com/api/siteViewCount";
 
-  //   fetch(apiURL)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setVisitors(data.body);
-  //     });
-  // }, []);
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setVisitors(data.body);
+      });
+  }, []);
 
   return (
     <footer
@@ -47,12 +41,12 @@ export default function Footer() {
         </div>
 
         <div className="mt-16 flex flex-col gap-2">
-          <p className="font-body type-preset-base">
+          <p className="font-body body-base">
             The number of visitors to this website is
           </p>
           <span
             id="visitors-count"
-            className="font-bold type-preset-4 text-blue-base"
+            className="font-title heading-3 text-blue-base"
           >
             {visitors}
           </span>
